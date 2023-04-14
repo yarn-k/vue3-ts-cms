30d48fea342ea8a85d716c57a0ecce5b743d548d
yarn <3039793769@qq.com>
2023-04-14 16:17:45 +0800

    配置pinia

--- vue3-ts-cms/src/views/main/main.vue
+++ vue3-ts-cms/src/views/main/main.vue
@@ -1,10 +1,17 @@
 <template>
     <div class="main">
-        <h2>主要1111</h2>
+        <h2>{{ counterPinia.counter }}--{{ counterPinia.doubleCounter }}</h2>
+        <button @click="counterClick">修改counter</button>
     </div>
 </template>
 
 <script setup lang="ts">
+import useCountPinia from '@/store/counter';
+const counterPinia = useCountPinia()
+
+function counterClick() {
+  counterPinia.changeCounterAction(1000)
+}
 
 </script>
 
